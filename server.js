db14806aae209d4748eba550c1afce2b899e3d80
Tolba-Adel <tolbaadel05@gmail.com>
2023-08-24 20:44:25 +0100

    initial author routes

--- server.js
+++ server.js
@@ -1,18 +1,23 @@
-if(process.env.NODE_ENV !== 'production'){
-    require('dotenv').config()
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
 }
 const express = require("express");
 const app = express();
 const expressLayouts = require("express-ejs-layouts");
-const indexRouter = require("./routes/index");
 const mongoose = require("mongoose");
+// const bodyParser = require('body-parser')
 
 app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
 app.set("layout", "layouts/layout");
 app.use(expressLayouts);
-app.set('views', __dirname + '/views')
 app.use(express.static("public"));
+// app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
+
+const indexRouter = require("./routes/index");
 app.use("/", indexRouter);
+const authorRouter = require("./routes/authors");
+app.use("/authors", authorRouter);
 
 mongoose.connect(process.env.DB_URL, {
   useNewUrlParser: true,
