1e6682d5ee6d2654dad7bf9db9d6e791a1ad8fc5
Tolba-Adel <tolbaadel05@gmail.com>
2023-08-26 21:01:44 +0100

    finished Author Routes

--- server.js
+++ server.js
@@ -5,21 +5,23 @@ const express = require("express");
 const app = express();
 const expressLayouts = require("express-ejs-layouts");
 const mongoose = require("mongoose");
-const bodyParser = require('body-parser')
+const bodyParser = require("body-parser");
+const methodOverride = require("method-override");
 
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
 app.set("layout", "layouts/layout");
 app.use(expressLayouts);
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
+app.use(methodOverride("_method"));
+app.use(bodyParser.urlencoded({ limit: "10mb", extended: false }));
 
 const indexRouter = require("./routes/index");
 app.use("/", indexRouter);
 const authorRouter = require("./routes/authors");
 app.use("/authors", authorRouter);
-const bookRouter = require('./routes/books')
-app.use('/books', bookRouter)
+const bookRouter = require("./routes/books");
+app.use("/books", bookRouter);
 
 mongoose.connect(process.env.DB_URL, {
   useNewUrlParser: true,
